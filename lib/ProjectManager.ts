866176482b050e96d8f5b364416886e107f7950f
Jean-Baptiste Ghoul <jbghoul@wanadev.fr>
2024-09-11 17:15:34 +0200

    define structures in project manager interface

--- lib/ProjectManager.ts
+++ lib/ProjectManager.ts
@@ -1,5 +1,8 @@
 import type Structure from "./Structure";
 export interface IProjectManager {
+
+    structures: { [id: string]: Structure };
+
     getLayer(layerName: string): Structure[];
 
     addStructure(structure: Structure, layerName?: string): void;
